23624a025923dedc8a7affee1dd25a53522cfc2f
LuiSxMendoza <juanluis123pit@gmail.com>
2022-02-17 21:32:49 -0600

    UP

--- Router.php
+++ Router.php
@@ -22,7 +22,7 @@ class Router {
         //? Proteger Rutas...
         session_start();
 
-        $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' : $_SERVER['REQUEST_URI'];
+        $currentUrl = ($_SERVER['REQUEST_URI'] === '') ? '/' : $_SERVER['REQUEST_URI'];
         $method = $_SERVER['REQUEST_METHOD'];
 
         $splitURL = explode('?', $currentUrl);
